0d4dc824288ca7fffca714cf0ed6db6bf9ad13da
HENRIQUE NOGUEIRA DOS SANTOS <henrique.nsantos1@senacsp.edu.br>
2022-09-14 22:21:12 -0300

    guardando informaçoes no banco de dados

--- vetor.php
+++ vetor.php
@@ -42,4 +42,13 @@ for($i = 0; $i < count($aluno); $i++) {
      </tr>";
    
 }
+
+
+foreach($aluno as $ind => $val){
+    echo "<tr>
+    <td>{$val['matricula']}</td>
+    <td>{$val['nome']}</td>
+    <td>{$val['semestre']}</td>
+    </tr>";
+}
 echo "</table>";
